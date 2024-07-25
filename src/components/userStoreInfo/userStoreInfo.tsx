6efa74fb8b7db286f0446f910df0f1ed28c29ec1
Eddy Juveno BOCQUELIN <bocquelin@gmail.com>
2024-07-25 16:10:28 +0300

    ajout info user storage

---
+++ src/components/userStoreInfo/userStoreInfo.tsx
@@ -0,0 +1,19 @@
+import './userStoreInfo.scss'
+
+const UserStoreInfo = ():JSX.Element =>{
+    return(
+        <div className="user-store-info">
+            <div className="store-info-card">
+
+            </div>
+            <div className="store-info-card">
+                
+            </div>
+            <div className="store-info-card">
+                
+            </div>
+        </div>
+    )
+}
+
+export default UserStoreInfo
--- src/pages/mainContent/mainContent.tsx
+++ src/pages/mainContent/mainContent.tsx
@@ -1,10 +1,15 @@
 import MainHeader from '../../components/mainHeader/mainHeader'
+import UserStoreInfo from '../../components/userStoreInfo/userStoreInfo'
 import './mainContent.scss'
 
 const MainContent = ():JSX.Element =>{
     return(
         <div className="main">
             <MainHeader/>
+            <UserStoreInfo/>
+            <div className="user-data">
+
+            </div>
         </div>
     )
 }
