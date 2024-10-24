2885b5fa5c63b31187e55b541fb9e72c895977ca
juven0 <bocquelin@gmail.com>
2024-10-24 16:11:42 +0300

    init login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,7 +4,12 @@ const Login = (): JSX.Element => {
   return (
     <div className="Login">
       <div className="login-content">
-        <div className="header"></div>
+        <div className="head">
+          <div className="logo">
+            <label htmlFor="">Nebula</label>
+          </div>
+        </div>
+        <div className="content"></div>
       </div>
       <div className="rigth-deco"></div>
     </div>
