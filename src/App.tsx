5371c8af7552374a91cd86c92b55fbd67289333a
juven0 <bocquelin@gmail.com>
2024-10-24 10:59:31 +0300

    init login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import UserHome from './pages/userHome/userHome'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import UserHome from "./pages/userHome/userHome";
+import Login from "./pages/login/login";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<UserHome/>}></Route>
+        <Route path="/home" element={<UserHome />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,14 @@
+import "./login.scss";
+
+const Login = (): JSX.Element => {
+  return (
+    <div className="Login">
+      <div className="login-content">
+        <div className="header"></div>
+      </div>
+      <div className="rigth-deco"></div>
+    </div>
+  );
+};
+
+export default Login;
