8cd00c2b467ec52336e740d4012aedbed1d45dbb
juven0 <bocquelin@gmail.com>
2024-10-29 11:44:51 +0300

    quick acces

--- src/components/QuickAcces/QuickAcces.tsx
+++ src/components/QuickAcces/QuickAcces.tsx
@@ -1,10 +1,33 @@
 import "./QuickAcces.scss";
 
+import imageicon from "../../assets/file_icons/image-document-svgrepo-com.svg";
+
 const QuickAcces = (): JSX.Element => {
   return (
     <div className="QuickAcces">
       <h4>Quick Acces</h4>
-      <div className="liste"></div>
+      <div className="liste">
+        <div className="item">
+          <div className="icon">
+            <img src={imageicon} alt="" />
+          </div>
+          <label htmlFor="">my image.png</label>
+        </div>
+
+        <div className="item">
+          <div className="icon">
+            <img src={imageicon} alt="" />
+          </div>
+          <label htmlFor="">my image.png</label>
+        </div>
+
+        <div className="item">
+          <div className="icon">
+            <img src={imageicon} alt="" />
+          </div>
+          <label htmlFor="">my image.png</label>
+        </div>
+      </div>
     </div>
   );
 };
