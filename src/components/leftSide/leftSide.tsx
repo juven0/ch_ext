e33b43213d3e34d4aa0043a7fbaa6d1b26d7232d
juven0 <bocquelin@gmail.com>
2024-11-05 10:45:32 +0300

    upload ok

--- src/components/leftSide/leftSide.tsx
+++ src/components/leftSide/leftSide.tsx
@@ -1,25 +1,80 @@
-import LeftNavs from '../leftNavs/leftNavs'
-import Logo from '../logo/logo'
-import Upload from '../../assets/icons/download-svgrepo-com.svg'
-import LogOut from '../../assets/icons/logout-2-svgrepo-com.svg'
+import LeftNavs from "../leftNavs/leftNavs";
+import Logo from "../logo/logo";
+import Upload from "../../assets/icons/download-svgrepo-com.svg";
+import LogOut from "../../assets/icons/logout-2-svgrepo-com.svg";
 
-import './leftSide.scss'
+import "./leftSide.scss";
+import { useState } from "react";
+import axios from "axios";
+import { useAppSelector } from "../../redux/hooks";
 
-const LeftSide = ():JSX.Element=>{
-    return(
-        <div className="left-side">
-            <Logo/>
-            <LeftNavs/>
-            <div className="upload">
-                <img src={Upload} alt="" />
-                <label htmlFor="">Import File</label>
-            </div>
-            <div className="logout">
-                <img src={LogOut} alt="" />
-                <label htmlFor="">Log Out</label>
-            </div>
+const LeftSide = (): JSX.Element => {
+  const user = useAppSelector((state) => state.user);
+  const [file, setFile] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    console.log("submit ");
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("userId", user.userId);
+    await axios
+      .post("http://localhost:3000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        onUploadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setUploadProgress(percentCompleted);
+        },
+      })
+      .then((res) => {});
+  };
+
+  return (
+    <div className="left-side">
+      <Logo />
+      <LeftNavs />
+      <div className="upload">
+        <label htmlFor="file">
+          <img src={Upload} alt="" />
+
+          <input
+            type="file"
+            onChange={(e) => handleFileChange(e)}
+            name="file"
+            id="file"
+          />
+        </label>
+      </div>
+      <div className="file_upload">
+        <button
+          type="button"
+          onClick={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          Upload
+        </button>
+        <div className="progress-bar-container">
+          {/* <div class="progress-bar" style="width: 50%;"></div> */}
         </div>
-    )
-}
+        <h3>{uploadProgress}</h3>
+      </div>
 
-export default LeftSide
+      <div className="logout">
+        <img src={LogOut} alt="" />
+        <label htmlFor="">Log Out</label>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSide;
--- src/pages/mainContent/mainContent.tsx
+++ src/pages/mainContent/mainContent.tsx
@@ -4,20 +4,24 @@ import Recent from "../../components/recent/Recent";
 import UserStoreInfo from "../../components/userStoreInfo/userStoreInfo";
 import "./mainContent.scss";
 import { useAppSelector } from "../../redux/hooks";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const MainContent = (): JSX.Element => {
   const user = useAppSelector((state) => state.user);
-  const files = null;
+  const [files, setFiles] = useState([]);
+  const [isLoad, setIsLoad] = useState(false);
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
+    setIsLoad(true);
     await axios
       .get(`http://localhost:3000/user/files/${user.userId}`)
       .then((res) => {
+        setFiles(res.data);
         console.log(res.data);
+        setIsLoad(false);
       });
   };
   return (
